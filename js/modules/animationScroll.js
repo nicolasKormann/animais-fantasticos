57872c2682767c34efff3f9ec9a805ec095a2f4d
Nícolas Kormann <nicolas.kormann@proton.me>
2023-10-02 12:45:13 -0300

    add animation scroll script

---
+++ js/modules/animationScroll.js
@@ -0,0 +1,22 @@
+export default function initAnimationScroll() {
+  const sections = document.querySelectorAll('[data-scroll="slidedown"]');
+
+  if (sections.length) {
+    const windowFraction = window.innerHeight * 0.7;
+
+    function handleScrollShow() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowFraction < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    handleScrollShow();
+    window.addEventListener("scroll", handleScrollShow);
+  }
+}
