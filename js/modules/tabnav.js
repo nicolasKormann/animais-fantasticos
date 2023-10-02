2be9770282db8fbb824b91aa0a3acdb7a7d3aa17
Nícolas Kormann <nicolas.kormann@proton.me>
2023-10-02 12:44:31 -0300

    add tabnav script

---
+++ js/modules/tabnav.js
@@ -0,0 +1,20 @@
+export default function initTabNav() {
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo", tabContent[index].dataset.anime);
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,14 @@
+import initScrollSmooth from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/animationScroll.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import { initModal, exitModal } from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+
+initScrollSmooth();
+initAnimationScroll();
+initAccordion();
+initTabNav();
+initModal();
+exitModal();
+initToolTip();
